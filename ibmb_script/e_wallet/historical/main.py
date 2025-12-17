8f7f1357227280b485e537dd138288b1cff71654
Daniel Satria <danielsatria31@gmail.com>
2025-12-17 17:49:56 +0000

    Initial pipeline for E-wallet

---
+++ ibmb_script/e_wallet/historical/main.py
@@ -0,0 +1,35 @@
+from utils.logger import logger
+from utils.historical import update_historical
+from utils.daily import update_flag_10min, update_flag_5mio
+
+job_title = "E-wallet"
+
+
+def main():
+    # Historical
+    logger.info("="*50)
+    logger.info(f"STEP 1: HISTORICAL {job_title} PROCESS")
+    logger.info("="*50)
+    logger.info(f"Running {job_title} Historical Process...")
+    update_historical()
+    logger.info(f"Historical {job_title} Historical Process succeeded.")
+
+    # 10min
+    logger.info("="*50)
+    logger.info("STEP 2: UPDATE FLAG 10MIN PROCESS")
+    logger.info("="*50)
+    logger.info("Running Update Flag 10min Process...")
+    update_flag_10min()
+    logger.info("Update Flag 10min Process succeeded.")
+
+    # 50mio
+    logger.info("="*50)
+    logger.info("STEP 3: UPDATE FLAG 50MIO PROCESS")
+    logger.info("="*50)
+    logger.info("Running Update Flag 50mio Process...")
+    update_flag_5mio()
+    logger.info("Update Flag 50mio Process succeeded.")
+
+
+if __name__ == "__main__":
+    main()
